4f6bc6127c87da3da0852ab335cac46a60e17c33
Guoyun Tian <guoyun812@qq.com>
2024-07-17 08:46:26 +0000

    add desc for keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,9 +4,14 @@
 --
 local map = vim.keymap.set
 -- map("n", "<leader>qq", "<cmd>:term<cr>", { desc = "Bash terminal" })
-map({ "n", "i" }, "<F5>", '<cmd>TermExec cmd="python %"<CR>')
-map({ "n", "i" }, "<leader>tt", function()
-  return table.concat({ "<cmd>.,.+", vim.v.count, "ToggleTerm<cr>" }, "")
-end, { expr = true })
-map({ "n", "i" }, "<leader>ts", "<cmd>TermSelect<CR>")
--- map({"n", "i"}, "<F5>", "<cmd>TermExec cmd=\"python %\"<CR>")
+map({ "n", "i" }, "<F5>", '<cmd>TermExec cmd="python %"<CR>', { desc = "Term Exec" })
+
+map({ "n", "i" }, "<C-t><C-t>", function()
+  return table.concat({ "<cmd>", vim.v.count, "ToggleTerm<cr>" }, "")
+end, { expr = true, desc = "Toggle Term" })
+
+map({ "n", "i" }, "<C-t><C-n>", function()
+  return table.concat({ "<cmd>", vim.v.count, "ToggleTerm dir=%:h<cr>" }, "")
+end, { expr = true, desc = "Toggle Term In Current Path" })
+
+map({ "n", "i" }, "<leader>ts", "<cmd>TermSelect<CR>", { desc = "Term Select" })
